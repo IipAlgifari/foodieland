84aa24ba59364fbe6af8258ec3db3fde638b20f9
IipALgifari <iipalgifari13@gmail.com>
2024-11-03 23:09:21 +0700

    Update Header burger-menu

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -3,15 +3,43 @@
 @description: Header
 --------------------------------------------------------------------------------- */
 
+import { Scrolllable } from "utilities";
+
 const Header = (() => {
-  // - handleSayHello
-  const handleSayHello = () => {
-    console.log("hello example");
+  // - handleShowNav
+  const handleShowNav = () => {
+    $(".js-burger-menu").on("click", (e) => {
+      const _this = $(e.currentTarget);
+      handleToggleMenu(_this, _this.hasClass("show"));
+    });
+  };
+
+  // - handleCloseNav
+  const handleCLoseNavigation = () => {
+    $(".js-burger-menu").on("click", (e) => {
+      const _this = $(e.currentTarget);
+      handleToggleMenu(_this, _this.hasClass("show"));
+    });
+  };
+
+  // - handleToggleMenu
+  const handleToggleMenu = (selector, status) => {
+    if (status) {
+      $("body").removeClass("show-menu");
+      selector.removeClass("show");
+      $(".js-header-nav").removeClass("show-navigation");
+      Scrolllable.enable();
+    } else {
+      $("body").addClass("show-menu");
+      selector.addClass("show");
+      $(".js-header-nav").addClass("show-Navigation");
+      Scrolllable.disable();
+    }
   };
 
   // - init
   const init = () => {
-    handleSayHello();
+    handleShowNav();
   };
 
   return {
